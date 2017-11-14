34d4b9c2d56d324101501158788077122fca792e
Guido Classen <clagix@gmail.com>
2017-11-14 15:44:39 +0100

    Enable Watchdog-LED

--- board/swarco/ccm2200/rootfs_overlay/etc/init.d/S99z-watchdog.sh
+++ board/swarco/ccm2200/rootfs_overlay/etc/init.d/S99z-watchdog.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
-#*****************************************************************************
-#* 
-#*  @file          S99z-watchdog.sh
-#*
-#*  /etc/init.d script to start watchdog trigger process
-#*
-#*  @version       1.0 (\$Revision: 189 $)
-#*  @author        Guido Classen <br>
-#*                 2008-2017 SWARCO Traffic Systems GmbH
-#* 
-#*  $LastChangedBy: clg9tr $  
-#*  $Date: 2009-05-26 16:27:12 +0200 (Di, 26 Mai 2009) $
-#*  $URL: svn://server-i/weiss-linux/buildroot/weiss_ch_conf/etc/init.d/S20z_usb_update $
-#*
-#*  @par Modification History:
-#*    2010-03-10 gc: initial version
-#*
-#*  
-#*****************************************************************************
+#
+# S99z-watchdog.sh
+#
+# Enable hardware watchdog and start trigger process
+#
+# Copyright (C) 2010-2017 by Weiss-Electronic GmbH
+#                            SWARCO Traffic Systems GmbH
+#                            Guido Classen <clagix@gmail.com>
+# All rights reserved.
+#     
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version
+# 2 or later as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Modification History:
+#    2010-03-10 gc: initial version
+#
 
+WATCHDOG_LED=15
 # internationalization functions for messages (identity)
 M_() {
   echo "$@"
@@ -39,6 +46,7 @@ start() {
     if [ $((`ccm2200_gpio_test /dev/ccm2200_gpio sconf` & 1)) != 0 ]; then
         
  	echo "Starting watchdog..."
+        ccm2200_watchdog /dev/ccm2200_watchdog led $((1<<$WATCHDOG_LED))
         /sbin/watchdog -t 1 /dev/ccm2200_watchdog
     else
         # dont start watchdog trigger process if CCM2200 is in service mode
@@ -46,7 +54,7 @@ start() {
         logger -t $0 "Watchdog disabled - CCM2200 in service mode"
         sys_mesg -n "watchdog" -e service-mode -p warning `M_ "CCM2200 in service mode. Watchdog disabled! Please set DIP-switches 4 and 5 to the OFF position." `
         /usr/bin/ccm2200_watchdog /dev/ccm2200_watchdog led 0x0000
-        echo 100 >/sys/class/leds/led15/brightness
+        echo 100 >/sys/class/leds/led$WATCHDOG_LED/brightness
     fi
 }	
 
