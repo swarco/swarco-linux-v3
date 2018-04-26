091115f6c3d85e3e85269da1ff6f2f893d41a2da
Guido Classen <clagix@gmail.com>
2018-04-26 17:18:03 +0200

    Enable busybox mdev features and exec /etc/network/if-add-* scripts

--- board/swarco/ccm2200/post-build.sh
+++ board/swarco/ccm2200/post-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # post build script for SWARCO Embedded Linux V3
-# 2017, "Guido Classen" <clagix@gmail.com>
+# 2017-2018, "Guido Classen"
 #
 
 cat >${TARGET_DIR}/etc/issue <<EOF 
@@ -9,9 +9,14 @@ Welcome to SWARCO Traffic Systems GmbH Embedded Linux v3
 Version: ${BR2_VERSION_FULL}   Build: `date '+%Y-%m-%d'`
 EOF
 
+# 2018-04-26 gc: run if-add-* scripts when new network device is plugged
+cat >>${TARGET_DIR}/etc/mdev.conf <<EOF 
+eth[0-9]+	root:root 660 @/bin/sh /etc/network/if-add-\$INTERFACE
+usb[0-9]+	root:root 660 @/bin/sh /etc/network/if-add-\$INTERFACE
+EOF
 
-# Disable unneeded services (can be enabled later using chmod a+x
-# /etc/init.d/Sxxxxx
+# Disable unneeded services (can be enabled later using 
+# chmod a+x /etc/init.d/Sxxxxx )
 #
 chmod a-x ${TARGET_DIR}/etc/init.d/S49ntp                       \
           ${TARGET_DIR}/etc/init.d/S50stunnel                   \
