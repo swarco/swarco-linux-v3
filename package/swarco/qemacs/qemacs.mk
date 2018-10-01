4cf1157a686b082c572908b93bffef27de8ab914
Guido Classen <clagix@gmail.com>
2018-10-01 18:10:00 +0200

    Fix: qemacs, use latest version from CVS

--- package/swarco/qemacs/qemacs.mk
+++ package/swarco/qemacs/qemacs.mk
@@ -5,8 +5,10 @@
 ################################################################################
 
 QEMACS_VERSION = 0.3.3
-QEMACS_SOURCE = qemacs-$(QEMACS_VERSION).tar.gz
-QEMACS_SITE = https://bellard.org/qemacs
+#QEMACS_SOURCE = qemacs-$(QEMACS_VERSION).tar.gz
+#QEMACS_SITE = https://bellard.org/qemacs
+QEMACS_SITE=cvs://cvs.savannah.nongnu.org:/sources/qemacs
+QEMACS_VERSION= HEAD
 QEMACS_LICENSE = LGPLv2.1+
 QEMACS_LICENSE_FILES = COPYING
 
@@ -14,30 +16,33 @@ QEMACS_LICENSE_FILES = COPYING
 #QEMACS_DEPENDENCIES = host-libaaa libbbb
 
 QEMACS_CONF_OPTS = \
-	--target=$(GNU_TARGET_NAME) \
-	--host=$(GNU_TARGET_NAME) \
-	--build=$(GNU_HOST_NAME) \
-	--prefix=/usr/ \
-	--includedir=$(STAGING_DIR)/include \
-	--oldincludedir=/usr/include \
-	--sysconfdir=$(TARGET_DIR)/etc \
+	--prefix=/usr \
+	--cross-prefix=$(TARGET_CROSS) \
+	--cpu=$(BR2_ARCH) \
 	--enable-tiny \
 	--disable-x11 \
 	--disable-xv \
+	--disable-xshm \
 	--disable-xrender \
 	--disable-html \
 	--disable-png \
-	--disable-plugins
+	--disable-plugins \
+	--disable-ffmpeg
 
 #CFLAGS=" -g -Os -pipe  -funsigned-char -falign-functions=0" 
 
 define QEMACS_CONFIGURE_CMDS
-	(cd $(@D) && $(TARGET_CONFIGURE_OPTS) ./configure $(QEMACS_CONF_OPTS))
+	( cd $(@D);                                     \
+		$(TARGET_CONFIGURE_OPTS)                \
+		$(TARGET_CONFIGURE_ARGS)                \
+		./configure                             \
+			$(QEMACS_CONF_OPTS);		\
+	)
 endef
 
 define QEMACS_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) CC=$(TARGET_CC)  \
-                STRIP="$(STRIPCMD) $(STRIP_STRIP_ALL)" 
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) V=1  \
+		LDFLAGS="-L$(STAGING_DIR)/usr/lib"
 endef
 
 define QEMACS_INSTALL_TARGET_CMDS
