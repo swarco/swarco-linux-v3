d34e9acdb4c8dabe23a41adaf614226c1f4eec83
Guido Classen <clagix@gmail.com>
2018-10-05 23:29:58 +0200

    Added Dockerfiles and build scripts for Dockerhub
    (reset containter user to root, so use with bamboo CI is possible)

---
+++ docker/images/docker_build.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -e -x
+id
+
+
+cd swarco-linux-v3
+
+# "clone" repository swarco-linux-v3-dl in exisiting directory "output"
+(
+    cd output
+    git init
+    git remote add origin https://github.com/swarco/swarco-linux-v3-dl
+    git pull --depth 1 origin master
+)
+
+make
+
+(
+    cd output/images
+    tar cJf ../../../swarco-linux-v3_images.tar.bz2 .
+)
+
+# cleanup build dir so it will not be part of the created Docker image
+(
+    cd output/build
+    for dir in *
+    do
+        if [ -d "$dir" ]; then
+            echo "cleanup directory $dir"
+            rm -rf "$dir"
+            mkdir "$dir"
+            # fake stamp files, so buildroot don't try to rebuild this
+            # directory
+            (cd "$dir"; touch .applied_patches_list         \
+                              .br_filelist_after            \
+                              .br_filelist_before           \
+                              .gitignore                    \
+                              .config                       \
+                              .stamp_built                  \
+                              .stamp_configured             \
+                              .stamp_downloaded             \
+                              .stamp_extracted              \
+                              .stamp_patched                \
+                              .stamp_staging_installed      \
+                              .stamp_kconfig_fixup_done     \
+                              .stamp_host_installed         \
+                              .stamp_target_installed )         
+        fi
+    done
+    mkdir -p linux-v3.4.113-ccm2200/arch/arm/boot/
+    cp -p  ../images/uImage linux-v3.4.113-ccm2200/arch/arm/boot/uImage
+)
+
+# remove download directory, so it will not be part of the created Docker image
+rm -rf output/dl output/.git
+
+#(
+    #    tar cJf ../swarco-linux-v2_staging_dir.tar.bz2 buildroot/buildroot-2.0/build_arm/staging_dir
+#)
+pwd
+# mv swarco-linux-v3/output/host .
+# ls -l
+
+# # remove swarco-linux-v3 build directory after build to prevent too
+# # large Docker image
+# rm -rf swarco-linux-v3
+
+# mkdir -p swarco-linux-v3/output
+# mv host swarco-linux-v3/output/
+
+ls -l
+cd ..
+
---
+++ docker/toolchain/docker_build.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -e -x
+id
+
+# make shallow clone
+git clone --depth 1 https://github.com/swarco/swarco-linux-v3
+
+cd swarco-linux-v3
+
+# make shallow clone
+git clone --depth 1 https://github.com/swarco/swarco-linux-v3-dl output
+
+make swarco_linux_v3_ccm2200_defconfig
+
+# disable toolchain locale support
+# sed -i -e "s/BR2_ENABLE_LOCALE=y/# BR2_ENABLE_LOCALE is not set/g" .config
+# grep ^BR2_ENABLE_LOCALE .config
+# make oldconfig
+# grep ^BR2_ENABLE_LOCALE .config
+# locale -a
+
+# substitute  grep -q -i utf8 in  grep -q -i utf-\\\?8
+sed -i -e 's/utf8/utf-\\\\\\?8/g' support/dependencies/dependencies.sh
+cat support/dependencies/dependencies.sh
+
+make toolchain
+
+# cleanup build dir so it will not be part of the created Docker image
+(
+    cd output/build
+    for dir in *
+    do
+        if [ -d "$dir" ]; then
+            echo "cleanup directory $dir"
+            rm -rf "$dir"
+            mkdir "$dir"
+            # fake stamp files, so buildroot don't try to rebuild this
+            # directory
+            (cd "$dir"; touch .applied_patches_list         \
+                              .br_filelist_after            \
+                              .br_filelist_before           \
+                              .gitignore                    \
+                              .config                       \
+                              .stamp_built                  \
+                              .stamp_configured             \
+                              .stamp_downloaded             \
+                              .stamp_extracted              \
+                              .stamp_patched                \
+                              .stamp_staging_installed      \
+                              .stamp_kconfig_fixup_done     \
+                              .stamp_host_installed         \
+                              .stamp_target_installed )         
+        fi
+    done
+)
+
+# remove download directory, so it will not be part of the created Docker image
+rm -rf output/dl output/.git
+
+#test cross compiler
+
+PATH=$PATH:$PWD/output/host/usr/bin/
+export PATH
+
+echo -e '#include <stdio.h>\nint main() { printf("Hello\\n"); }' >hello.c
+pwd
+ls -l
+
+arm-buildroot-linux-gnueabi-gcc -o hello hello.c
+
+
+
+echo Oki Doki
+
