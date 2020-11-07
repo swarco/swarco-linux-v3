18a9ce62c549748111cef91b58dafddcf8d4255a
Guido Classen <clagix@gmail.com>
2020-11-07 01:42:50 +0100

    Use board/swarco/common/initramfs for scc-air-mk2 and 5port SCC

--- board/swarco/scc-air-v2/post-image.sh
+++ board/swarco/scc-air-v2/post-image.sh
@@ -6,11 +6,11 @@ set -x
 
 # Rename output files with name needed for USB stick update
 if [ -f $BINARIES_DIR/rootfs.ubifs ] ; then
-    mv -f $BINARIES_DIR/rootfs.ubifs  $BINARIES_DIR/rootfs-scc-air-v2.arm.ubifs
+    mv -f $BINARIES_DIR/rootfs.ubifs  $BINARIES_DIR/rootfs-scc-air-mk2.arm.ubifs
 fi
 if [ -f $BINARIES_DIR/zImage ] ; then
     cat  $BINARIES_DIR/zImage \
          $BINARIES_DIR/sama5d35-swarco-scc-air-mk2.dtb  \
-         >$BINARIES_DIR/zImage-scc-air-v2-ubifs.bin
+         >$BINARIES_DIR/zImage-scc-air-mk2-ubifs.bin
     rm $BINARIES_DIR/zImage
 fi
